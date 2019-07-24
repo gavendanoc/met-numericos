048405625f94e667ecef328c936ba47aae71dbd1
Ernesto <ebastidasp@unal.edu.co>
2019-07-24 15:18:58 -0500

    mc

--- proyectoFinal/Prueba_Proyecto_Redes.m
+++ proyectoFinal/Prueba_Proyecto_Redes.m
@@ -5,7 +5,7 @@ clear;
 clc;
 re=300000;      % Número de Reynolds
 e=0.002;        % Rugosidad Absoluta
-d=0.1;          % Diámetro interno
+d=0.5;          % Diámetro interno
 
 % variables de la ecuacion HW
 L = 50;
@@ -19,9 +19,12 @@ b=10;
 
 % Proponemos un valor de f inicial para que entre al ciclo
 f=1;
+
+cont = 1;
+
 while abs(f)>0.00001
     % Calculamos el valor de C
-    c=(a+b)./2;
+    c=(a+b)./2;   
     % Evaluamos en C
     fa=1/sqrt(c);
     fb=-2*(log((e/d)/3.71+2.51/(re*sqrt(c))))*0.434294481903252000;
@@ -30,13 +33,22 @@ while abs(f)>0.00001
         b=c;
     else
         a=c;
-    end   
+    end
+    Vf(cont) = c;
+    cont = cont + 1;  
 end
 
 
 
 fprintf('Factor de Pérdida de carga =%8.9f \n', c)
 fprintf('Velocidad del agua por el tubo =%8.9f \n', (2* hf*d*g/(c*L))^0.5 )
+
+Vv = (2* hf*d*g./(Vf*L)).^0.5;
+
+plot(Vf,Vv, 'o');
+xlabel('F');
+ylabel('Velocidad');
+ title('Gráfica velocidad en función de f');
 
 
 
